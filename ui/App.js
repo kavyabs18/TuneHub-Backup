5ba0a6bed11ebfff5bb10e0516b160ce03d60ae6
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-03-07 09:22:40 +0530

    TuneHub ui

--- ui/App.js
+++ ui/App.js
@@ -2,26 +2,18 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const App = () => {
-//get request -string
-const getDataFromBackend = async () => {
-  const response = await axios.get("http://localhost:8080/call");
-  console.log(response.data);
-  document.getElementById("para").innerHTML = response.data;
-};
-
-//post request - string
-const data = "Hello";
-const postDataFromFrontend = async () => {
-  const response = await axios.post("http://localhost:8080/testpost", {data});
-  console.log(response.data);
-  document.getElementById("para").innerHTML = response.data;
-};
-
 //post request-form
-const[formData, setFormData] = useState("");
+const[songName, setSongName] = useState("");
+const[songArtist, setSongArtist] = useState("");
+const[songGenre, setSongGenre] = useState("");
+const[songLink, setSongLink] = useState("");
+
 const postFormFromFrontend = async () => {
   const response = await axios.post("http://localhost:8080/testform", {
-    formData,
+    songName,
+    songArtist,
+    songGenre,
+    songLink,
   });
   console.log(response.data);
   document.getElementById("para").innerHTML = response.data;
@@ -29,21 +21,44 @@ const postFormFromFrontend = async () => {
 
   return (
   <div className="App">
-    <button onClick={getDataFromBackend}>Get Data</button>
-    <p id="para"></p>
-
-    <button onClick={postDataFromFrontend}>Post Data</button>
-    <p id="para"></p>
-
     <form onSubmit={postFormFromFrontend}>
-      <label>Enter song name</label><br></br><br></br>
+      <label>Song Name:</label><br></br><br></br>
       <input 
         type="text" 
-        name="formData" 
-        value={formData} 
-        onChange={(e) => setFormData(e.target.value)}
+        name="songName" 
+        placeholder="Enter Song Name"
+        value={songName} 
+        onChange={(e) => setSongName(e.target.value)}
       ></input><br></br><br></br>
-      <input type="submit" value="testForm"></input>
+
+      <label>Song Artist:</label><br></br><br></br>
+      <input 
+        type="text" 
+        name="songArtist" 
+        placeholder="Enter Song Artist"
+        value={songArtist} 
+        onChange={(e) => setSongArtist(e.target.value)}
+      ></input><br></br><br></br>
+
+      <label>Song Genre:</label><br></br><br></br>
+      <input 
+        type="text" 
+        name="songGenre" 
+        placeholder="Enter Song Genre"
+        value={songGenre} 
+        onChange={(e) => setSongGenre(e.target.value)}
+      ></input><br></br><br></br>
+
+      <label>Song Link:</label><br></br><br></br>
+      <input 
+        type="text" 
+        name="songLink" 
+        placeholder="Enter Song Link"
+        value={songLink} 
+        onChange={(e) => setSongLink(e.target.value)}
+      ></input><br></br><br></br>
+
+      <input type="submit" value="ADD SONG"></input>
     </form>
     </div>
   );
